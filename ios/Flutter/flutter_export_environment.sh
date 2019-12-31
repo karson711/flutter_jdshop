16295f8ca73095e6a7ff2639f2b3013d5ae3db1f
anfa <anfa@anfadeMacBook-Pro-5.local>
2019-12-31 09:46:51 +0800

    iphone真机正常运行

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/anfa/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anfa/Flutter学习/flutter_jdshop"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/anfa/flutter_jdshop"
+export "FLUTTER_TARGET=/Users/anfa/flutter_jdshop/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/anfa/Documents/flutter/bin/cache/artifacts/engine/ios"
